2cf02b9556c9f09f954fc249ff698ad6f2f6cbd8
Dan Ramnauth <DanR1606@outlook.com>
2017-09-18 16:17:44 +1000

    Set-Up all classes

--- src/Pong.java
+++
@@ -1,9 +0,0 @@
-/*
- * Developed by Dhanveer Ramnauth;
- */
-
-public class Pong {
-
-	
-	
-}
---
+++ src/com/alkira/main/Pong.java
@@ -0,0 +1,8 @@
+package com.alkira.main;
+
+/*
+ * Developed by Dhanveer Ramnauth.
+ */
+
+public class Pong {
+}
